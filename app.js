b90bd6314d8063871fea422dd7bd6d8fa56ff0c0
Jussi Vesala <jussi.vesala@reaktor.fi>
2014-10-04 21:13:00 +0300

    more debug

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ function handleSetCommand(message) {
 var deviceEventListener = telldus.addDeviceEventListener(function(deviceId, status) {
   logger.debug('received event for device ' + deviceId + ' status: ' + status.name  + ' value: ' + status.value);
   var value = 0
+  console.log("status %j", status)
   if (status.name === 'ON' || status.name === 'OFF') {
     if (isDimmer(deviceId)) value = (status.name === 'ON' ? "ff" : 0)
     else value = (status.name === 'ON' ? 1 : 0)
